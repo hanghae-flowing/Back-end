caa95f233066b754b45457c6d7a6b35080c1afc0
john <john3210of@gmail.com>
2022-03-10 23:27:33 +0900

    test

--- src/test/java/com/pjt/flowing/FlowingApplicationTests.java
+++ src/test/java/com/pjt/flowing/FlowingApplicationTests.java
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
-package com.pjt.flowing;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FlowingApplicationTests {
-//    @Test
-//    void contextLoads() {
-//    }
-
-}
-=======
+//
 //package com.pjt.flowing;
 //
 //import org.junit.jupiter.api.Test;
@@ -19,9 +6,9 @@ class FlowingApplicationTests {
 //
 //@SpringBootTest
 //class FlowingApplicationTests {
-//    @Test
-//    void contextLoads() {
-//    }
+////    @Test
+////    void contextLoads() {
+////    }
 //
 //}
->>>>>>> 0f197d0f4505e4513befc160e7073872fa875e1f
+//
