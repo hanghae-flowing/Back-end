0f197d0f4505e4513befc160e7073872fa875e1f
jeonho <jhkang1995@naver.com>
2022-03-10 23:15:56 +0900

    feat: health api

--- src/main/java/com/pjt/flowing/controller/HealthController.java
+++ src/main/java/com/pjt/flowing/controller/HealthController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
     @GetMapping("/health")
     public String healthCheck(){
-        return "I'm still alive";
+        return "I'm still alive~!!!";
     }
 }
