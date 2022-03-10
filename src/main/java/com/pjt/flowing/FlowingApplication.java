7792b18cd68d27474da0d3623fda9e896fa79dcb
jeonho <jhkang1995@naver.com>
2022-03-10 22:45:30 +0900

    chore: CICD pipeline 구축

--- src/main/java/com/pjt/flowing/FlowingApplication.java
+++ src/main/java/com/pjt/flowing/FlowingApplication.java
@@ -1,33 +1,20 @@
 package com.pjt.flowing;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
-@Slf4j
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class FlowingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FlowingApplication.class, args);
-
     }
-    @Value("${spring.datasource.url}")
-    private String url;
-
-    @Value("${spring.datasource.username}")
-    private String username;
-
-    @Value("${spring.datasource.password}")
-    private String password;
 
     @PostConstruct
-    public void test(){
-        log.info("{}",url);
-        log.info("{}",username);
-        log.info("{}",password);
+    public void setup(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
-
 }
