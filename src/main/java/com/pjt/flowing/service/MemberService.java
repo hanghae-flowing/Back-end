4fad1040a810e689e73062fec093c1bef9b180cb
john <john3210of@gmail.com>
2022-03-10 23:39:22 +0900

    FIX : ip address config

--- src/main/java/com/pjt/flowing/service/MemberService.java
+++ src/main/java/com/pjt/flowing/service/MemberService.java
@@ -92,8 +92,8 @@ public class MemberService {
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", kakao_api);
-        body.add("redirect_uri", "http://localhost:8080/member/kakao/callback");
-//        body.add("redirect_uri", "http://localhost:3000/member/kakao/callback");
+//        body.add("redirect_uri", "http://localhost:8080/member/kakao/callback");
+        body.add("redirect_uri", "http://localhost:3000/member/kakao/callback");
         body.add("code", code);
 
         // HTTP 요청 보내기
