db1aa17ac31385c68ff0521a60c6b5decacf02a8
jeonho <jhkang1995@naver.com>
2022-03-10 22:23:34 +0900

    FIX : 환경변수 주입

--- src/main/java/com/pjt/flowing/FlowingApplication.java
+++ src/main/java/com/pjt/flowing/FlowingApplication.java
@@ -1,13 +1,33 @@
 package com.pjt.flowing;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+@Slf4j
 @SpringBootApplication
 public class FlowingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FlowingApplication.class, args);
+
+    }
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @PostConstruct
+    public void test(){
+        log.info("{}",url);
+        log.info("{}",username);
+        log.info("{}",password);
     }
 
 }
