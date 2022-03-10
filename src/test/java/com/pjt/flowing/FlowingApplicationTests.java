78fbf9d65e6e6929f5afb9b406301fc87aa0e446
jeonho <jhkang1995@naver.com>
2022-03-10 22:25:18 +0900

    fix: 테스트코드 주석처리

--- src/test/java/com/pjt/flowing/FlowingApplicationTests.java
+++ src/test/java/com/pjt/flowing/FlowingApplicationTests.java
@@ -1,12 +1,12 @@
-package com.pjt.flowing;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FlowingApplicationTests {
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.pjt.flowing;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FlowingApplicationTests {
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
