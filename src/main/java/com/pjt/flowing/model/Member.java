06aad2151d27f848a88bc7d221e1f02d756ba247
john <john3210of@gmail.com>
2022-03-14 21:22:26 +0900

    test

--- src/main/java/com/pjt/flowing/model/Member.java
+++ src/main/java/com/pjt/flowing/model/Member.java
@@ -14,7 +14,7 @@ public class Member extends Timestamped {
 
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id //pk값으로 쓰겠다
-    @Column(name = "userId")
+    @Column
     private Long id;
 
     @Column(name="kakao_id",nullable = false, unique = true)
