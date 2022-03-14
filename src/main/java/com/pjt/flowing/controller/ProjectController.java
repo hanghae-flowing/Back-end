eeb582cdaf6a8760473c3beaced9cca571bae7cf
jeonho <jhkang1995@naver.com>
2022-03-14 16:00:51 +0900

    Auto stash before rebase of "origin/develop"

--- src/main/java/com/pjt/flowing/controller/ProjectController.java
+++ src/main/java/com/pjt/flowing/controller/ProjectController.java
@@ -1,8 +1,21 @@
 package com.pjt.flowing.controller;
 
+import com.pjt.flowing.dto.ProjectResponseDto;
+import com.pjt.flowing.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+@RequiredArgsConstructor
 @RestController
 public class ProjectController {
+    private final ProjectService projectService;
+    @GetMapping("/api/post")
+    public List<ProjectResponseDto> getProject(){
+        List<ProjectResponseDto> response = projectService.getAll();
+        return response;
+    }
 
 }
