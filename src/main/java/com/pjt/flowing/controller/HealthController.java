2b4b26df1b97c33cfa368f8feead2d8ce9527800
jeonho <jhkang1995@naver.com>
2022-03-14 12:17:16 +0900

    fix: import localdate

--- src/main/java/com/pjt/flowing/controller/HealthController.java
+++ src/main/java/com/pjt/flowing/controller/HealthController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 public class HealthController {
     @GetMapping("/health")
