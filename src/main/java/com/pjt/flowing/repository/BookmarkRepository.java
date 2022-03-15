f927b3f788924b81c8b58ba0e3eace0bc83b5126
jeonho <jhkang1995@naver.com>
2022-03-15 12:22:52 +0900

    test: project조회

--- src/main/java/com/pjt/flowing/repository/BookmarkRepository.java
+++ src/main/java/com/pjt/flowing/repository/BookmarkRepository.java
@@ -1,7 +1,12 @@
 package com.pjt.flowing.repository;
 
 import com.pjt.flowing.model.Bookmark;
+import com.pjt.flowing.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+    //List<Bookmark> findAllByMember_IdOrderByModifiedAtDesc(Long userId);
+    List<Project> findAllByMember_IdOrderByModifiedAtDesc(Long UserId);
 }
--- src/main/java/com/pjt/flowing/service/ProjectService.java
+++ src/main/java/com/pjt/flowing/service/ProjectService.java
@@ -2,7 +2,9 @@ package com.pjt.flowing.service;
 
 
 import com.pjt.flowing.dto.ProjectResponseDto;
+import com.pjt.flowing.model.Bookmark;
 import com.pjt.flowing.model.Project;
+import com.pjt.flowing.repository.BookmarkRepository;
 import com.pjt.flowing.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,10 +18,23 @@ import java.util.stream.Collectors;
 @Transactional(readOnly = true)
 public class ProjectService {
     private final ProjectRepository projectRepository;
+    private final BookmarkRepository bookmarkRepository;
 
     public List<ProjectResponseDto> getAll(Long userId){
         List<Project> all = projectRepository.findAllByMember_IdOrderByModifiedAtDesc(userId);
         List<ProjectResponseDto> dto = all.stream()
+                .map(ProjectResponseDto::from)
+                .collect(Collectors.toList());
+        return dto;
+    }
+
+    public List<ProjectResponseDto> get4(Long userId){
+        //List<Project> all = projectRepository.findAllByMember_IdOrderByModifiedAtDesc(userId);
+        List<Project> bookmarked = bookmarkRepository.findAllByMember_IdOrderByModifiedAtDesc(userId);
+        for(int i = 0 ;i<4;i++){
+            System.out.println(bookmarked.get(i));
+        }
+        List<ProjectResponseDto> dto = bookmarked.stream()
                 .map(ProjectResponseDto::from)
                 .collect(Collectors.toList());
         return dto;
