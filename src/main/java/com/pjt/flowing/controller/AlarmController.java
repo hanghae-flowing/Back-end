3db4035e33a52fe9904c5a49769f27bdc6962b72
kimhyojin <gywls1474@nate.com>
2022-03-22 13:27:33 +0900

    feat: 알람기능 수정2

--- src/main/java/com/pjt/flowing/controller/AlarmController.java
+++ src/main/java/com/pjt/flowing/controller/AlarmController.java
@@ -6,20 +6,23 @@ import com.pjt.flowing.dto.MsgResponseDto;
 import com.pjt.flowing.model.Member;
 import com.pjt.flowing.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class AlarmController {
 
     private final MemberRepository memberRepository;
     private final SimpMessageSendingOperations messagingTemplates;
 
-    @PostMapping("/invite")
-    public MsgResponseDto inviteProject(@RequestBody InviteRequestDto requestDto) {
+    @MessageMapping("/invite/{userId}")
+    public MsgResponseDto inviteProject(@Payload InviteRequestDto requestDto) {
 
         MsgResponseDto msgResponseDto = new MsgResponseDto();
         // 초대하려는 이메일이 존재하지 않을 때 !!
@@ -29,15 +32,12 @@ public class AlarmController {
         }
 
         // 초대하려는 이메일이 존재할 때 !!
-        Member sender = memberRepository.findById(requestDto.getSenderId()).orElseThrow(
-                () -> new IllegalArgumentException("아이디가 존재하지 않습니다.")
-        );
         Member receiver = memberRepository.findByEmail(requestDto.getEmail());
 
         // 프론트에게 전해줄 보내는 자의 Id와 보내고 싶은 사용자 Id를 담아서 전해준다.
         InviteResponseDto inviteResponseDto = new InviteResponseDto();
-        inviteResponseDto.setSenderId(sender.getId());
-        inviteResponseDto.setReceiverid(receiver.getId());
+        inviteResponseDto.setSenderId(requestDto.getSenderId());
+        inviteResponseDto.setReceiverId(receiver.getId());
 
         messagingTemplates.convertAndSend("/sub/invite" + receiver.getId(), inviteResponseDto);
         msgResponseDto.setMsg("초대완료");
--- src/main/java/com/pjt/flowing/dto/InviteResponseDto.java
+++ src/main/java/com/pjt/flowing/dto/InviteResponseDto.java
@@ -10,5 +10,5 @@ import lombok.Setter;
 public class InviteResponseDto {
 
     private Long senderId;
-    private Long receiverid;
+    private Long receiverId;
 }
