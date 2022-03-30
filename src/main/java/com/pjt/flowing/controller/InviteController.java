a78de7d9c5fa8f7bd68a47a4ab4b9d6c3164fcd2
jeonho <jhkang1995@naver.com>
2022-03-30 17:44:19 +0900

    feat: 초대하기 관련 api 구현
    초대하기, 초대 조회, 초대 삭제, 초대 수락, 초대 거절

---
+++ src/main/java/com/pjt/flowing/controller/InviteController.java
@@ -0,0 +1,36 @@
+package com.pjt.flowing.controller;
+
+import com.pjt.flowing.dto.request.InviteRequestDto;
+import com.pjt.flowing.service.InviteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class InviteController {
+    private final InviteService inviteService;
+    //초대하기.
+    @PostMapping("/inviting")
+    public String inviteMember(@RequestBody InviteRequestDto inviteRequestDto){
+        return inviteService.inviteMember(inviteRequestDto);
+    }
+
+    //초대받은 목록 확인하기.
+    @GetMapping("/inviting/{userId}")
+    public String getInviting(@PathVariable Long userId){
+        return inviteService.getInviting(userId);
+    }
+
+    //초대 수락하기.
+    @DeleteMapping("/accepting/{invitingId}")
+    public String acceptInviting(@PathVariable Long invitingId){
+        return inviteService.accept(invitingId);
+    }
+
+    //초대 거절하기
+    @DeleteMapping("/refusing/{invitingId}")
+    public String refuseInviting(@PathVariable Long invitingId){
+        return inviteService.refuse(invitingId);
+    }
+
+}
