27816ad4ca488745b95e2b88a9337b82cb22df8f
jeonho <jhkang1995@naver.com>
2022-04-04 16:19:10 +0900

    feat: 폴더 정보 조회하기 api

---
+++ src/main/java/com/pjt/flowing/dto/request/FolderGetRequestDto.java
@@ -0,0 +1,6 @@
+package com.pjt.flowing.dto.request;
+
+public class FolderGetRequestDto {
+    Long folderTableId;
+    Long userId;
+}
--- src/main/java/com/pjt/flowing/repository/FolderTableRepository.java
+++ src/main/java/com/pjt/flowing/repository/FolderTableRepository.java
@@ -3,5 +3,8 @@ package com.pjt.flowing.repository;
 import com.pjt.flowing.model.FolderTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FolderTableRepository extends JpaRepository<FolderTable,Long> {
+    List<FolderTable> findAllByMember_Id(Long userId);
 }
