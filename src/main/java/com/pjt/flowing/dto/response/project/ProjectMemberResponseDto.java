40a4612dc69bd2b231efdf3ca28185634e49da36
john <john3210of@gmail.com>
2022-04-06 15:04:39 +0900

    fix : project detail에서 멤버리스트 받아오기

---
+++ src/main/java/com/pjt/flowing/dto/response/project/ProjectMemberResponseDto.java
@@ -0,0 +1,17 @@
+package com.pjt.flowing.dto.response.project;
+
+import lombok.Getter;
+
+@Getter
+public class ProjectMemberResponseDto {
+    private Long userId;
+    private String nickName;
+    private String profileImageUrl;
+
+
+    public ProjectMemberResponseDto(Long userId, String nickName, String profileImageUrl) {
+        this.userId = userId;
+        this.nickName = nickName;
+        this.profileImageUrl = profileImageUrl;
+    }
+}
