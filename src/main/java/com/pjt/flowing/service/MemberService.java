9cb60cc152729e881832065bfe457531fced0eb8
john <john3210of@gmail.com>
2022-04-07 17:40:26 +0900

    fix : cors, redirect uri 도메인 준비

--- src/main/java/com/pjt/flowing/service/MemberService.java
+++ src/main/java/com/pjt/flowing/service/MemberService.java
@@ -91,7 +91,8 @@ public class MemberService {
         body.add("grant_type", "authorization_code");
         body.add("client_id", kakao_api);
 //        body.add("redirect_uri", "http://localhost:3000/member/kakao/callback");
-        body.add("redirect_uri", "http://hanghae-final5.s3-website.ap-northeast-2.amazonaws.com/member/kakao/callback");
+//        body.add("redirect_uri", "http://hanghae-final5.s3-website.ap-northeast-2.amazonaws.com/member/kakao/callback");
+        body.add("redirect_uri", "https://kirini.co.kr/member/kakao/callback");
         body.add("code", code);
 
         // HTTP 요청 보내기
